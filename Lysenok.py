44580827baf93f9dd60ad1459b8a695291b12587
mark <agarkovkirill76@gmail.com>
2022-10-19 19:59:57 +0300

    task 1731: adding Lysenok.py for git lesson

---
+++ Lysenok.py
@@ -0,0 +1,21 @@
+numbers = int(input())
+
+if (numbers <= 50):
+    print ('gj')
+elif ((numbers > 50)and(numbers < 100)):
+    print ('sad')
+else:
+    print ('qd')
+if (numbers > 100):
+    print ('ds')
+
+
+
+
+
+
+
+
+
+
+
